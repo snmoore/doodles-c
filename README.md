8e90c089a9d8be1b3a947dc8929e1c5b5462d588
Simon Moore <simon@snmoore.net>
2015-08-06 08:40:12 +0100

    Added function to convert a string to an integer

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Programming doodles in C
 
-## fibonacci
-Generate the first 13 terms of the Fibonacci sequence:
-    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+## atoi
+Convert a string to an integer
 
 ## binary_search
 Find the position of a target value (a key) in a sorted array using a binary search.
+
+## fibonacci
+Generate the first 13 terms of the Fibonacci sequence:
+    0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 
 ## reverse
 Reverse words and sentences.
@@ -14,4 +17,4 @@ Reverse words and sentences.
 Round up an integer to the next highest power of 2
 
 ## stack
-A stack implemented using a linked list.
+A stack implemented using a linked list
---
+++ atoi/atoi.c
@@ -0,0 +1,51 @@
+// Convert a string to an integer
+//
+// See http://pubs.opengroup.org/onlinepubs/009695399/functions/atoi.html
+
+#include <ctype.h>  // For isdigit
+#include <stdio.h>  // For printf
+#include <stdlib.h> // For EXIT_SUCCESS, NULL, atoi
+
+#define NELEMENTS(a)    (sizeof(a) / sizeof(a[0]))
+
+// Convert a string to an integer
+int my_atoi(const char *str) {
+    int sign  = 1;
+    int value = 0;
+
+    if(str != NULL) {
+        // Process the sign, if present
+        if(*str == '-') {
+            sign = -1;
+            str++;
+        }
+        else if(*str == '+') {
+            str++;
+        }
+
+        // Convert the digits
+        while(isdigit(*str)) {
+            value *= 10;
+            value += *str - '0';
+            str++;
+        }
+    }
+
+    return value * sign;
+}
+
+int main(void) {
+    char *strings[] = {              /* INT_MAX */
+            "0", "11", "+11", "-11", "2147483647", "abc", ""
+    };
+
+    printf("String\tMy atio\tatoi\n");
+    for(size_t i = 0; i < NELEMENTS(strings); i++) {
+        printf("\"%s\" %d %d\n", strings[i], my_atoi(strings[i]), atoi(strings[i]));
+    }
+
+    // Note that atoi fails with NULL e.g. causes a segmentation fault
+    printf("NULL %d SEGV\n", my_atoi(NULL));
+
+    return EXIT_SUCCESS;
+}
