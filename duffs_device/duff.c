f9babe46ccae7582f99a7b0e6841d8c0195b8a8a
Simon Moore <simon@snmoore.net>
2015-11-08 13:48:45 +0000

    Clean-up

--- duffs_device/duff.c
+++ duffs_device/duff.c
@@ -7,8 +7,7 @@
 #define BYTES (1024*1024*400)
 
 // Duff's Device in standard form
-static void duff(volatile short int *to, volatile short int *from, int count)
-{
+static void duff(volatile short int *to, volatile short int *from, int count) {
     // Set some markers that can be checked later
     from[0]       = 1;
     from[count-1] = 2;
@@ -37,8 +36,8 @@ static void duff(volatile short int *to, volatile short int *from, int count)
     printf("%lu usec\n", Stop - Start);
 }
 
-static void loop(short int *to, short int *from, int count)
-{
+// Loop alternative to Duff's Device
+static void loop(short int *to, short int *from, int count) {
     // Set some markers that can be checked later
     from[0]       = 3;
     from[count-1] = 4;
@@ -59,59 +58,30 @@ static void loop(short int *to, short int *from, int count)
     printf("%lu usec\n", Stop - Start);
 }
 
-static uint32_t builtin(void *to, void *from, size_t bytes)
-{
+// Builtin alternative to Duff's Device, using memcpy
+static void builtin(short int *to, short int *from, size_t bytes) {
+    // Set some markers that can be checked later
+    int end = bytes/sizeof(from[0])-1;
+    from[0] = 5;
+    from[end] = 6;
+    printf("Memcpy: [%hd %hd]\n", from[0], from[end]);
 
+    // Copy using memcpy, with time stamps before and after
+    clock_t Start = clock();
+    memcpy(to, from, bytes);
+    clock_t Stop = clock();
+
+    // Check the markers to verify the code was not optimized away
+    printf("Memcpy: [%hd %hd] ", from[0], from[end]);
+
+    // Check the execution time
+    printf("%lu usec\n", Stop - Start);
 }
 
-static void runner(uint32_t (*function)(void *to, void *from, size_t bytes), char* label, unsigned int iterations, uint8_t *to, uint8_t *from, size_t bytes)
-{
-    // Set some markers at each end that can be checked later
-    static uint8_t marker = 1;
-    from[0]       = marker++;
-    from[bytes-1] = marker++;
+int main(void) {
+    // Assume failure
+    int result = EXIT_FAILURE;
 
-    // Execute the function for the desired number of iterations
-    for(unsigned int i = 0; i < iterations; i++)
-    {
-        printf("\n%s %d", label, i);
-
-        /* Execute the function and measure the time taken, excluding call overhead */
-        uint32_t usec = function((void *)to, (void *)from, bytes);
-
-        /* Check the markers to ensure data was actually copied */
-        if((to[0] != from[0]) || (to[bytes-1] != from[bytes-1]))
-        {
-            printf("data not correctly copied\n");
-            break;
-        }
-
-        printf("%u usec\n", usec);
-    }
-}
-
-//static void builtin(short int *to, short int *from, size_t bytes)
-//{
-//    // Set some markers that can be checked later
-//    int end = bytes/sizeof(from[0])-1;
-//    from[0] = 5;
-//    from[end] = 6;
-//    printf("Memcpy: [%hd %hd]\n", from[0], from[end]);
-//
-//    // Copy using memcpy, with time stamps before and after
-//    clock_t Start = clock();
-//    memcpy(to, from, bytes);
-//    clock_t Stop = clock();
-//
-//    // Check the markers to verify the code was not optimized away
-//    printf("Memcpy: [%hd %hd] ", from[0], from[end]);
-//
-//    // Check the execution time
-//    printf("%lu usec\n", Stop - Start);
-//}
-
-int main(void)
-{
     // Allocate some large source and destination buffers on the heap
     short int *Src = (short int *)malloc(BYTES);
     short int *Dst = (short int *)malloc(BYTES);
@@ -131,20 +101,19 @@ int main(void)
             loop(Dst, Src, BYTES / sizeof(short int));
         }
 
-        // Run the memcpy test 10 times
+        // Run the builtin test 10 times
         for(i = 0; i < 10; i++)
         {
             builtin(Dst, Src, BYTES);
         }
 
-        // Free the memory
-        free(Src);
-        free(Dst);
+        // Success
+        result = EXIT_SUCCESS;
+    }
 
-        return EXIT_SUCCESS;
-    }
-    else
-    {
-        return EXIT_FAILURE;
-    }
+    // Free the buffers
+    free(Src);
+    free(Dst);
+
+    return result;
 }
