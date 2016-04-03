2a5a87a452f58d74e3bc9e49ff1cec5dd4236d5b
Simon Moore <simon@snmoore.net>
2016-04-03 12:40:14 +0100

    Added method to count using a lookup table

--- count_bits/count_bits.c
+++ count_bits/count_bits.c
@@ -2,8 +2,9 @@
 //
 // See https://graphics.stanford.edu/~seander/bithacks.html
 
-#include <stdio.h>  // For printf
-#include <stdlib.h> // For EXIT_SUCCESS
+#include <stdbool.h>    // For bool, true, false
+#include <stdio.h>      // For printf
+#include <stdlib.h>     // For EXIT_SUCCESS
 
 #define NELEMENTS(a)    (sizeof(a) / sizeof(a[0]))
 
@@ -27,19 +28,33 @@ unsigned int kernighan(unsigned int value) {
     return count;
 }
 
+// Count using a lookup table
+unsigned int lookup(unsigned int value) {
+    // Lookup table that will contain the number of bits in all bytes 0..255
+    static unsigned int bits[256]; // is initialized to all zeros
+
+    // Only build the array once
+    if(!bits[1]) {
+        // Compute the number of bits set in all byte values 0..255
+        for(unsigned int i = 0; i < 256; i++) {
+            bits[i] = (i & 1) + bits[i/2];
+        }
+    }
+
+    // Add up the number of bits from each of the 4 bytes in the 32-bit value
+    return bits[ value        & 0xff] +
+           bits[(value >>  8) & 0xff] +
+           bits[(value >> 16) & 0xff] +
+           bits[(value >> 24) & 0xff];
+}
+
 int main(void) {
     unsigned int values[] = { 0, 1, 2, 3, 4, 5, 7, 8, 10, 64, 128, 255 };
 
-    // Count the number of bits set using the obvious method
-    printf("\nObvious method:\n");
+    printf("Value  Obvious  Kernighan  Lookup\n");
     for(size_t i = 0; i < NELEMENTS(values); i++) {
-        printf("Value: %3u, bits: %u\n", values[i], obvious(values[i]));
-    }
-
-    // Count the number of bits set using the obvious method
-    printf("\nKernighan's method:\n");
-    for(size_t i = 0; i < NELEMENTS(values); i++) {
-        printf("Value: %3u, bits: %u\n", values[i], kernighan(values[i]));
+        printf("%-3u    %-2u       %-2u         %-2u\n", values[i],
+               obvious(values[i]), kernighan(values[i]), lookup(values[i]));
     }
 
     return EXIT_SUCCESS;
