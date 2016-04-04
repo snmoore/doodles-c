9985cfd5e3cf3c659837c58e69e850a452fde996
Simon Moore <simon@snmoore.net>
2016-04-04 15:22:34 +0100

    Fix the recursion and tidy up

--- quick_select/quick_select.c
+++ quick_select/quick_select.c
@@ -13,49 +13,6 @@
 
 #define NELEMENTS(a)    (sizeof(a)/sizeof(a[0]))
 
-// Find the median in an array of values using quickselect
-
-int quickselect(int* data, size_t nelements, size_t lo, size_t hi) {
-    if(lo < hi) {
-        // Select the rightmost element as the pivot
-        int pivot = data[hi];
-
-        // Partition using Lomuto partitioning
-        size_t partition = lo;
-        for(size_t i = lo; i < hi; i++) {
-            if(data[i] <= pivot) {
-                // Swap the element with the partition
-                int temp        = data[i];
-                data[i]         = data[partition];
-                data[partition] = temp;
-
-                // Move the partition rightwards
-                partition++;
-            }
-        }
-        // Swap the pivot with the partition
-        data[hi]        = data[partition];
-        data[partition] = pivot;
-
-        // Reached the middle of the array?
-        if(partition == nelements/2) {
-            return data[nelements/2];
-        }
-        else if(partition < nelements/2) {
-            // Recurse into the left sub-array
-            return quickselect(data, nelements, lo, partition - 1);
-        }
-        else {
-            // Recurse into the right sub-array
-            return quickselect(data, nelements, partition + 1, hi);
-        }
-    }
-    else {
-        // Only one element left
-        return data[lo];
-    }
-}
-
 // Print an array of values
 void print(const char* msg, const int *data, size_t nelements) {
     if(data != NULL) {
@@ -70,9 +27,57 @@ void print(const char* msg, const int *data, size_t nelements) {
     }
 }
 
+// Find the median in an array of values using quickselect
+int quickselect(int* data, size_t nelements, size_t lo, size_t hi) {
+    if(lo < hi) {
+        // Select the rightmost element as the pivot
+        int pivot = data[hi];
+
+        // Partition using Lomuto partitioning
+        size_t partition = lo;
+        for(size_t i = lo; i < hi; i++) {
+            // Move smaller elements to the left
+            if(data[i] <= pivot) {
+                // Swap the element with the partition
+                int temp        = data[i];
+                data[i]         = data[partition];
+                data[partition] = temp;
+
+                // Move the partition rightwards
+                partition++;
+            }
+        }
+
+        // Swap the pivot with the partition
+        data[hi]        = data[partition];
+        data[partition] = pivot;
+
+        print("During:        ", data, nelements);
+
+        // Is the partition at the mid-point?
+        if(partition == nelements/2) {
+            return data[nelements/2];
+        }
+        // Is the partition to the right of the mid-point?
+        else if(partition > nelements/2) {
+            // Recurse into the left partition
+            return quickselect(data, nelements, lo, partition - 1);
+        }
+        // Is the partition to the left of the mid-point?
+        else {
+            // Recurse into the right partition
+            return quickselect(data, nelements, partition + 1, hi);
+        }
+    }
+    else {
+        // Only one element left
+        return data[lo];
+    }
+}
+
 int main(void) {
     // Create an array of unsorted data
-    int data[] = { 23, 21, 76, 16, 52, 43 };
+    int data[] = { 23, 21, 76, 16, 43, 52, 18 };
     print("Unsorted:      ", data, NELEMENTS(data));
 
     // Find the median using the quick select algorithm
