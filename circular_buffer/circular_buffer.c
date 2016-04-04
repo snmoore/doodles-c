5b016a887ce267366cf6c52e6018acf6e27b21ae
Simon Moore <simon@snmoore.net>
2016-04-04 10:45:34 +0100

    Refine the circular buffer implementation

--- circular_buffer/circular_buffer.c
+++ circular_buffer/circular_buffer.c
@@ -2,169 +2,231 @@
 
 #include <stdbool.h>    // For bool, true, false
 #include <stdio.h>      // For printf
-#include <stdlib.h>     // For malloc, NULL, EXIT_FAILURE, EXIT_SUCCESS
-#include <string.h>     // For memcpy
+#include <stdlib.h>     // For calloc, NULL, EXIT_FAILURE, EXIT_SUCCESS, size_t
+#include <string.h>     // For memcpy, memset
 
+// A circular buffer storing chars
 typedef struct circular_t {
-    int head;
-    int tail;
-    int capacity;
-    char* buffer;
+    size_t capacity;    // capacity of the buffer i.e. total number of elements
+    size_t occupied;    // number of elements currently occupied
+    size_t head;        // index from which to read
+    size_t tail;        // index at which to write
+    char*  buffer;      // buffer allocated on the heap
 } circular_t;
 
-// Create a circular buffer
-circular_t* create(unsigned int nelements) {
-    // Allocate the structure
-    circular_t* circular = malloc(sizeof(circular_t));
-    if(circular == NULL) {
+// Create a new circular buffer
+circular_t* create(size_t capacity) {
+    if(capacity == 0) {
+        printf("Bad argument\n");
         return NULL;
     }
 
-    // Allocate the buffer
-    circular->buffer = malloc(nelements * sizeof(char));
-    if(circular->buffer == NULL) {
+    // Allocate memory for the circular buffer
+    circular_t* circular = malloc(sizeof(circular_t));
+    if(circular == NULL) {
+        printf("Failed to allocate struct\n");
+        return NULL;
+    }
+    circular->buffer = calloc(capacity, sizeof(char));
+    if(circular == NULL) {
+        printf("Failed to allocate buffer\n");
         free(circular);
         return NULL;
     }
 
-    circular->head = 0;
-    circular->tail = 0;
-    circular->capacity = nelements;
+    // Initialize the parameters
+    circular->capacity = capacity;
+    circular->occupied = 0;
+    circular->head     = 0;
+    circular->tail     = 0;
 
     return circular;
 }
 
 // Destroy a circular buffer
 void destroy(circular_t** circular) {
-    if(circular != NULL) {
-        // Free the buffer
-        free((*circular)->buffer);
-
-        // Free the structure
-        free(*circular);
+    if((circular == NULL) || (*circular == NULL)) {
+        printf("Bad circular buffer\n");
+        return;
     }
+
+    free((*circular)->buffer);
+    free(*circular);
+    circular = NULL;
 }
 
-// How many free elements are available in the buffer?
-int available(circular_t* circular) {
+// Read a single element from the head
+char read(circular_t* circular) {
     if(circular == NULL) {
+        printf("Bad circular buffer\n");
         return 0;
     }
 
-    // If the buffer is completely empty
-    if(circular->head == circular->tail) {
-        return circular->capacity;
+    if(circular->occupied > 0) {
+        // Read the element
+        char c = circular->buffer[circular->head];
+
+        // Clear the element
+        circular->buffer[circular->head] = ' ';
+
+        // Update the parameters
+        circular->occupied--;
+        circular->head++;
+        circular->head %= circular->capacity;
+
+        return c;
     }
-    // If the buffer is partly full, and is not wrapped around
-    else if(circular->head < circular->tail) {
-        return circular->capacity - (circular->tail - circular->head);
-    }
-    // If the buffer is partly full, and is wrapped around
     else {
-        return circular->head - circular->tail;
+        printf("Buffer is empty\n");
+        return 0;
     }
 }
 
-// Write some data to the tail of a circular buffer
-bool write(circular_t* circular, char* data, int nelements) {
-    if(circular == NULL) {
-        printf("Bad argument\n");
-        return false;
+// Read many at a time from the head
+size_t read_many(circular_t* circular, char* data, size_t nelements) {
+    if((circular == NULL) || (data == NULL) || (nelements == 0)) {
+        printf("Bad arguments\n");
+        return 0;
     }
 
-    if(nelements > available(circular)) {
-        printf("Not enough free elements\n");
-        return false;
+    if(nelements > circular->occupied) {
+        printf("Truncating\n");
+        nelements = circular->occupied;
     }
 
-    printf("Appending %d elements: %s\n", nelements, data);
+    if(nelements > 0) {
+        // [..H..T..]
+        if(circular->head < circular->tail) {
+            memcpy(data, circular->buffer + circular->head, nelements * sizeof(data[0]));
+            memset(circular->buffer + circular->head, ' ', nelements * sizeof(data[0]));
+        }
+        // [..T..H..] without wrap-around
+        else if((circular->capacity - circular->head) > nelements) {
+            memcpy(data, circular->buffer + circular->head, nelements * sizeof(data[0]));
+            memset(circular->buffer + circular->head, ' ', nelements * sizeof(data[0]));
+        }
+        // [..T..H..] with wrap-around
+        else {
+            // Elements to the right of the head
+            size_t relements = circular->capacity - circular->head;
+            memcpy(data, circular->buffer + circular->head, relements * sizeof(data[0]));
+            memset(circular->buffer + circular->head, ' ', relements * sizeof(data[0]));
 
-    // Append without wrap-around
-    if(circular->capacity - circular->tail > nelements) {
-        memcpy(circular->buffer + circular->tail, data, nelements * sizeof(char));
-        circular->tail += nelements;
+            // Elements to the left of the tail
+            size_t lelements = nelements - relements;
+            memcpy(data + relements, circular->buffer, lelements * sizeof(data[0]));
+            memset(circular->buffer, ' ', lelements * sizeof(data[0]));
+        }
+
+        // Update the parameters
+        circular->occupied -= nelements;
+        circular->head += nelements;
+        circular->head %= circular->capacity;
     }
-    // Append with wrap-around
     else {
-        // Elements at the right side of the buffer
-        int right = circular->capacity - circular->tail;
-        memcpy(circular->buffer + circular->tail, data, right * sizeof(char));
-
-        // Elements at the left side of the buffer
-        int left = nelements - right;
-        memcpy(circular->buffer, data+right, left * sizeof(char));
-
-        circular->tail += nelements;
-        circular->tail %= circular->capacity;   // can just use -= for efficiency
+        printf("Buffer is empty\n");
     }
 
-    return true;
+    // Return the number of elements actually read
+    return nelements;
 }
 
-// Read some elements from the head of a circular buffer
-bool read(circular_t* circular, int nelements) {
+// Write a single element to the tail
+void write(circular_t* circular, char c) {
     if(circular == NULL) {
-        printf("Bad argument\n");
-        return false;
+        printf("Bad circular buffer\n");
+        return;
     }
 
-    if(nelements > circular->capacity - available(circular)) {
-        printf("Not enough elements to read\n");
-        return false;
-    }
+    if((circular->capacity - circular->occupied) > 0) {
+        // Write the element
+        circular->buffer[circular->tail] = c;
 
-    // Remove without wrap-around
-    printf("Remove %d elements: %.*s\n", nelements, nelements, circular->buffer + circular->head);
-    if(circular->capacity - circular->head > nelements) {
-        memset(circular->buffer + circular->head, ' ', nelements * sizeof(char));
-        circular->head += nelements;
+        // Update the parameters
+        circular->occupied++;
+        circular->tail++;
+        circular->tail %= circular->capacity;
     }
-    // Remove with wrap-around
     else {
-        // Elements at the right side of the buffer
-        int right = circular->capacity - circular->head;
-        memset(circular->buffer + circular->head, ' ', right * sizeof(char));
+        printf("Buffer is full\n");
+    }
+}
 
-        // Elements at the left side of the buffer
-        int left = nelements - right;
-        memset(circular->buffer, ' ', left * sizeof(char));
-
-        circular->head += nelements;
-        circular->head %= circular->capacity;   // can just use -= for efficiency
+// Write many at a time to the tail
+size_t write_many(circular_t* circular, const char* data, size_t nelements) {
+    if((circular == NULL) || (data == NULL) || (nelements == 0)) {
+        printf("Bad arguments\n");
+        return 0;
     }
 
-    return true;
+    if(nelements > (circular->capacity - circular->occupied)) {
+        printf("Truncating\n");
+        nelements = (circular->capacity - circular->occupied);
+    }
+
+    if(nelements > 0) {
+        // [..T..H..]
+        if(circular->tail < circular->head) {
+            memcpy(circular->buffer + circular->tail, data, nelements * sizeof(data[0]));
+        }
+        // [..H..T..] without wrap-around
+        else if((circular->capacity - circular->tail) > nelements) {
+            memcpy(circular->buffer + circular->tail, data, nelements * sizeof(data[0]));
+        }
+        // [..H..T..] with wrap-around
+        else {
+            // Elements to the right of the tail
+            size_t relements = circular->capacity - circular->tail;
+            memcpy(circular->buffer + circular->tail, data, relements * sizeof(data[0]));
+
+            // Elements to the left of the head
+            size_t lelements = nelements - relements;
+            memcpy(circular->buffer, data + relements, lelements * sizeof(data[0]));
+        }
+
+        // Update the parameters
+        circular->occupied += nelements;
+        circular->tail     += nelements;
+        circular->tail     %= circular->capacity;
+    }
+    else {
+        printf("Buffer is full\n");
+    }
+
+    // Return the number of elements actually written
+    return nelements;
 }
 
 // Print a representation of a circular buffer
 void print(circular_t* circular) {
     if(circular == NULL) {
-        printf("Bad argument\n");
+        printf("Bad circular buffer\n");
+        return;
     }
 
-    printf("Capacity:  %2d\n", circular->capacity);
-    printf("Available: %2d\n", available(circular));
-    printf("Head:      %2d\n", circular->head);
-    printf("Tail:      %2d\n", circular->tail);
+    printf("Capacity: %2lu\n", circular->capacity);
+    printf("Occupied: %2lu\n", circular->occupied);
+    printf("Head:     %2lu\n", circular->head);
+    printf("Tail:     %2lu\n", circular->tail);
 
     printf("Contents:\n");
-    for(int i = 0; i < circular->capacity; i++) {
-        printf("%2d ", i);
+    for(size_t i = 0; i < circular->capacity; i++) {
+        printf("%2lu ", i);
     }
     printf("\n");
-    for(int i = 0; i < circular->capacity; i++) {
+    for(size_t i = 0; i < circular->capacity; i++) {
         printf("%2c ", circular->buffer[i]);
     }
     printf("\n");
-    for(int i = 0; i < circular->capacity; i++) {
+    for(size_t i = 0; i < circular->capacity; i++) {
         if(circular->head == i) {
-            printf("%*s", i*3+2, "H");
+            printf("%*s", (int)i*3+2, "H");
         }
     }
     printf("\n");
-    for(int i = 0; i < circular->capacity; i++) {
+    for(size_t i = 0; i < circular->capacity; i++) {
         if(circular->tail == i) {
-            printf("%*s", i*3+2, "T");
+            printf("%*s", (int)i*3+2, "T");
         }
     }
     printf("\n");
@@ -179,133 +241,126 @@ int main(void) {
     }
     print(circular);
 
-    // Write some data to the tail
-    printf("Write some data to the tail:\n");
-    int i;
-    for(i = 0; i < 10; i++) {
-        char c = 'a' + i;
-        write(circular, &c, 1);
+    // Read when the buffer is empty
+    printf("\nRead when the buffer is empty: ");
+    char c = read(circular);
+    if(c != 0) {
+        printf("Bad read\n");
     }
     print(circular);
 
-    // Write some more data to the tail
-    printf("Write some more data to the tail:\n");
-    char* data = "klmnopq";
-    write(circular, data, strlen(data));
+    // Write one at a time to the tail
+    printf("\nWrite one at a time to the tail: ");
+    for(int i = 0; i < 10; i++) {
+        char c = 'a' + i;
+        write(circular, c);
+        printf("%c ", c);
+    }
+    printf("\n");
     print(circular);
 
-    // Try to write more data than there is space available
-    printf("Try to write more data than there is space available:\n");
-    data = "rstuvwxyz";
-    write(circular, data, strlen(data));
+    // Read one at a time from the head
+    printf("\nRead one at a time from the head: ");
+    for(int i = 0; i < 5; i++) {
+        char c = read(circular);
+        printf("%c ", c);
+    }
+    printf("\n");
     print(circular);
 
-    // Try to read more data than exist in the buffer
-    printf("Try to read more data than exist in the buffer:\n");
-    read(circular, 19);
+    // Write one at a time to the tail and wrap-around
+    printf("\nWrite one at a time to the tail and wrap-around: ");
+    for(int i = 10; i < 24; i++) {
+        char c = 'a' + i;
+        write(circular, c);
+        printf("%c ", c);
+    }
+    printf("\n");
     print(circular);
 
-    // Read some data from the head
-    printf("Read some data from the head:\n");
-    read(circular, 10);
+    // Read one at a time from the head and wrap-around
+    printf("\nRead one at a time from the head and wrap-around: ");
+    for(int i = 0; i < 17; i++) {
+        char c = read(circular);
+        printf("%c ", c);
+    }
+    printf("\n");
     print(circular);
 
-    // Write some more data to the tail, with wrap-around
-    printf("Write some more data to the tail, with wrap-around:\n");
-    data = "rstuvwxyz";
-    write(circular, data, strlen(data));
+    // Write one at a time to the tail and hit the head
+    printf("\nWrite one at a time to the tail and hit the head: ");
+    for(int i = 0; i < 20; i++) {
+        char c = 'a' + i;
+        write(circular, c);
+        printf("%c ", c);
+    }
+    printf("\n");
     print(circular);
 
-//    // Completely fill the buffer
-//    printf("Completely fill the buffer:\n");
-//    data = "xy";
-//    write(circular, data, strlen(data));
-//    print(circular);
+    // Read one at a time from the head and hit the tail
+    printf("\nRead one at a time from the head and hit the tail: ");
+    for(int i = 0; i < 22; i++) {
+        char c = read(circular);
+        printf("%c ", c);
+    }
+    printf("\n");
+    print(circular);
 
-    // Read some data from the head, with wrap-around
-    printf("Read some data from the head, with wrap-around:\n");
-    read(circular, 12);
+    // Start anew
+    destroy(&circular);
+    printf("\nStart anew:\n");
+    circular = create(20);
+    if(circular == NULL) {
+        return EXIT_FAILURE;
+    }
+    print(circular);
+
+    // Write many at a time to the tail
+    printf("\nWrite many at a time to the tail: ");
+    const char* data = "abcdefghijklmno";
+    size_t count = write_many(circular, data, strlen(data));
+    printf("%s (%zu elements)\n", data, count);
+    print(circular);
+
+    // Read many at a time from the head
+    printf("\nRead many at a time from the head: ");
+    char buffer[11];
+    count = read_many(circular, buffer, 10);
+    buffer[10] = '\0';
+    printf("%s (%zu elements)\n", buffer, count);
+    print(circular);
+
+    // Write many at a time to the tail and wrap-around
+    printf("\nWrite many at a time to the tail and wrap-around: ");
+    const char* data2 = "pqrstuvxwyz";
+    count = write_many(circular, data2, strlen(data2));
+    printf("%s (%zu elements)\n", data2, count);
+    print(circular);
+
+    // Read many at a time from the head and wrap-around
+    printf("\nRead many at a time from the head and wrap-around: ");
+    char buffer2[15];
+    count = read_many(circular, buffer2, 14);
+    buffer2[14] = '\0';
+    printf("%s (%zu elements)\n", buffer2, count);
+    print(circular);
+
+    // Write many at a time to the tail and hit the head
+    printf("\nWrite many at a time to the tail and hit the head: ");
+    const char* data3 = "abcdefghijklmnopqrstuvwxyz";
+    count = write_many(circular, data3, strlen(data3));
+    printf("%s (%zu elements)\n", data3, count);
+    print(circular);
+
+    // Read many at a time from the head and hit the tail
+    printf("\nRead many at a time from the head and hit the tail: ");
+    char buffer3[27];
+    count = read_many(circular, buffer3, 26);
+    buffer3[26] = '\0';
+    printf("%s (%zu elements)\n", buffer3, count);
     print(circular);
 
     // Clean-up
     destroy(&circular);
     return EXIT_SUCCESS;
 }
-
-
-
-
-//Ring buffer / Circular buffer
-//1024 bytes.
-//
-//Linear memory view case 1:
-//+----------------------------------------------+
-//|NNNN          DDDDDDDDDDNNNNNNNNNN         end|
-//+----------------------------------------------+
-//           Tail^         ^ Head
-//Linear memory view case 2:
-//+----------------------------------------------+
-//|DDDDDDDDDD                              DDDDDD|
-//+----------------------------------------------+
-//           ^Head                         ^Tail
-//
-//Some new data: pointnewData:
-//+-----------+
-//|DDDDDDDDDDD|
-//+-----------+
-//
-//
-//bool appendToBuf(const char *newData, size_t newDataSize)
-//// is free space
-////    head idx/ptr
-////    tail idx/prt
-////        size - (head - tail) but take into account wrap around
-//                if head > tail    ==> case 1
-//                    if buflen - (head - tail) > newDataSize
-//                        have enough space
-//                    else
-//                        fail
-//
-//                if head < tail    ==> case 2
-//                    left = head
-//                    rigth = buflen - tail
-//                    freespace = right - left
-//                    if freespace > newDataSize
-//
-//                    if tail - head > newDataSize
-//                    //if (buflen - tail) - head > newDataSize
-//                        have enough space
-//                    else
-//                        fail
-//
-//
-//                if head > tail    ==> case 1
-////                    if buflen - head > newDataSize    ==> case 1
-////                        mempcy(head, newData, newDataSize)
-////                    else
-////                        fit = buflen - head
-//                          mempcy(head, newData, fit)
-//                          mempcy(start, newData, newDataSize -fit)
-//
-//                if head < tail    ==> case 2
-//                    if tail - head > newDataSize
-//                        mempcy(head, newData, newDataSize)
-//
-////            if head > tail    ==> case 1
-////                if tail > newDataSize
-////                    simple memcpy
-////                else
-////                    part of data at left, part of data at right, resulting head < tail
-////            if head < tail    ==> case 2
-////                     simple memcpy
-//
-//
-//// if it fits
-////     memcpy(tail, newData, newDataSize) but take into account wrap around
-////     update tail
-////     return true
-//
-//// if it does not fit
-////    return false
-////    assume someone else frees newData
-
