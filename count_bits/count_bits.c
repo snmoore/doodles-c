374a18e59e323c23027057fb43e519640f13234b
Simon Moore <simon@snmoore.net>
2024-11-03 15:31:44 +0000

    Refine count bits: count bits in a byte using a compact lookup table

--- count_bits/count_bits.c
+++ count_bits/count_bits.c
@@ -3,6 +3,7 @@
 // See https://graphics.stanford.edu/~seander/bithacks.html
 
 #include <stdbool.h>    // For bool, true, false
+#include <stdint.h>     // For uint8_t
 #include <stdio.h>      // For printf
 #include <stdlib.h>     // For EXIT_SUCCESS
 
@@ -48,13 +49,36 @@ unsigned int lookup(unsigned int value) {
            bits[(value >> 24) & 0xff];
 }
 
-int main(void) {
-    unsigned int values[] = { 0, 1, 2, 3, 4, 5, 7, 8, 10, 64, 128, 255 };
+// Count bits in a byte using a compact lookup table
+uint8_t compact(uint8_t value) {
+    static uint8_t bits[] = {
+        0,  // 0    0   0b0000
+        1,  // 1    1   0b0001
+        1,  // 2    2   0b0010
+        2,  // 3    3   0b0011
+        1,  // 4    4   0b0100
+        2,  // 5    5   0b0101
+        2,  // 6    6   0b0110
+        3,  // 7    7   0b0111
+        1,  // 8    8   0b1000
+        2,  // 9    9   0b1001
+        2,  // 10   A   0b1010
+        3,  // 11   B   0b1011
+        2,  // 12   C   0b1100
+        3,  // 13   D   0b1101
+        3,  // 14   E   0b1110
+        4,  // 15   F   0b1111
+    };
+    return bits[value >> 4] + bits[value & 0x0f];
+}
 
-    printf("Value  Obvious  Kernighan  Lookup\n");
+int main(void) {
+    unsigned int values[] = { 0, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 64, 128, 255 };
+
+    printf("Value  Obvious  Kernighan  Lookup  Compact\n");
     for(size_t i = 0; i < NELEMENTS(values); i++) {
-        printf("%-3u    %-2u       %-2u         %-2u\n", values[i],
-               obvious(values[i]), kernighan(values[i]), lookup(values[i]));
+        printf("%-3u    %-2u       %-2u         %-2u         %-2u\n", values[i],
+               obvious(values[i]), kernighan(values[i]), lookup(values[i]), compact(values[i]));
     }
 
     return EXIT_SUCCESS;
